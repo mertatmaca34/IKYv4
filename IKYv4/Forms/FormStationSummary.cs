2b7fdb3a21009e1fdec7d19c9b2fa6778a1cf9e8
Mert Atmaca <mertatmaca34@gmail.com>
2024-09-20 10:02:47 +0300

    ++

--- IKYv4/Forms/FormStationSummary.cs
+++ IKYv4/Forms/FormStationSummary.cs
@@ -2,6 +2,7 @@
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace IKYv4.Forms
@@ -9,16 +10,25 @@ namespace IKYv4.Forms
     public partial class FormStationSummary : Form
     {
         private readonly IKadroDurumlariManager _kadroDurumlariManager;
+        private readonly ISeflikManager _seflikManager;
+        private readonly string _stationName;
 
-
-        public FormStationSummary(IKadroDurumlariManager kadroDurumlariManager)
+        public FormStationSummary(IKadroDurumlariManager kadroDurumlariManager, string stationName)
         {
             _kadroDurumlariManager = kadroDurumlariManager;
+            _stationName = stationName;
         }
 
         private void FormStationSummary_Load(object sender, EventArgs e)
         {
+            var seflik = _seflikManager.GetAll(s => s.SeflikAdi == _stationName).Data.FirstOrDefault();
 
+            var kadroDurumlari = _kadroDurumlariManager.GetAll(k => k.SeflikId == seflik.Id).Data;
+
+            foreach (var kadro in kadroDurumlari)
+            {
+                kadro.
+            }
         }
     }
 }
